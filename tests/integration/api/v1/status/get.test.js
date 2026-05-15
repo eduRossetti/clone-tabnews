11b493e289b4fd7fb3108e0f8288ab70ed8fdf9e
edurossetti <edu.rossetti100@gmail.com>
2026-05-15 19:57:19 -0300

    adicionando novos testes no get.test.js

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,12 +1,17 @@
 test("GET to /api/v1/status shoud return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
+  expect(response.headers.get("content-type")).toContain("application/json");
+  
 
   const responseBody = await response.json();
   expect(responseBody.updated_at).toBeDefined();
 
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+
+  expect(responseBody.dependencies).toBeDefined();
+  expect(typeof responseBody.dependencies).toBe("object");
 
   const version = responseBody.dependencies.database.version;
   expect(version).toBeDefined();

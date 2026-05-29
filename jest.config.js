4c345adcd848cbddc620331fadd920ae3b6c36c5
edurossetti <edu.rossetti100@gmail.com>
2026-05-28 21:41:39 -0300

    adicionando o novo endpoint migrations

---
+++ jest.config.js
@@ -0,0 +1,16 @@
+const nextJest = require("next/jest");
+const dotenv = require("dotenv");
+dotenv.config({
+    path: ".env.development",
+}) // seta que o jest deve olhar para o env de "development" e não de "test"
+
+const createJestConfig = nextJest({
+    dir: ".",
+});
+
+const jestConfig = createJestConfig({
+    moduleDirectories: ["node_modules", "<rootDir>"],
+    
+});
+
+module.exports = jestConfig;
---
+++ pages/api/v1/migrations/index.js
@@ -0,0 +1,48 @@
+
+import migrationRunner from 'node-pg-migrate';
+import { log } from 'node:console';
+import { join } from "node:path";
+import database from "infra/database";
+
+export default async function migrations(request, response) {
+  const dbClient = await database.getNewClient();
+
+  const defaultMigrationsOptions = {
+    dbClient: dbClient,
+    databaseUrl: process.env.DATABASE_URL, // credenciais de acesso do banco
+    dryRun: true, // se vai rodar de verdade ou só vai testar o que rodaria
+    dir: join("infra", "migrations"), //caminho de onde estão as migrations
+    direction: "up",
+    verbose: true, //mostra mais informações(SQL) sobre as migrations quando executada
+    migrationsTable: "pgmigrations" //qual tabela vai ser usada pra controlar as migrations
+  }
+  
+  if(request.method === "GET"){
+    const pendingMigrations = await migrationRunner(defaultMigrationsOptions);
+    await dbClient.end();
+
+  return response.status(200).json(pendingMigrations)
+  }
+
+  if(request.method === "POST"){
+
+    const migratedMigrations = await migrationRunner({
+      ...defaultMigrationsOptions,
+      dryRun: false,
+    });
+
+    await dbClient.end();
+
+    if(migratedMigrations.length > 0){
+      return response.status(201).json(migratedMigrations);
+    }
+    
+
+  return response.status(200).json(migratedMigrations);
+  // retorna um array com todas as migrations que foram executadas
+  }
+
+
+  response.status(405).end();
+}
+

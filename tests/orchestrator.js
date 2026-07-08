2cc4a839cadb8a0a45dc32c12eec6cf26a85e4c2
edurossetti <edu.rossetti100@gmail.com>
2026-07-08 20:50:44 -0300

    fix delete.test.js to CI and fix other little things

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -4,6 +4,7 @@ async function waitForAllServices() {
   await waitForWebServer();
 
   async function waitForWebServer() {
+    // Tenta acessar a rota de status repetidas vezes até o servidor estar pronto
     return retry(fetchStatusPage, {
       retries: 100,
       maxTimeout: 1000,
@@ -12,6 +13,7 @@ async function waitForAllServices() {
     async function fetchStatusPage() {
       const response = await fetch("http://localhost:3000/api/v1/status");
 
+      // Se a resposta não for 200 (OK), lança erro para tentar de novo
       if (response.status !== 200) {
         throw Error();
       }
@@ -19,8 +21,8 @@ async function waitForAllServices() {
   }
 }
 
-const orhcestrator = {
+const orchestrator = {
   waitForAllServices,
 };
 
-export default orhcestrator;
+export default orchestrator;
